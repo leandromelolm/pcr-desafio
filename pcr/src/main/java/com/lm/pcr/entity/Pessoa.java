c06a049dc8f975503c765e4aad6aba70bd6b15f3
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-03-18 18:46:29 -0300

    Profile dev: mensagem de texto por celular com Telesign funcionando. Profile dev: mobile text message with Telesign working.

--- pcr/src/main/java/com/lm/pcr/entity/Pessoa.java
+++ pcr/src/main/java/com/lm/pcr/entity/Pessoa.java
@@ -2,18 +2,15 @@ package com.lm.pcr.entity;
 
 import java.util.Objects;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "pessoas")
 public class Pessoa{
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="pessoa_sequence")
+	@SequenceGenerator(name="pessoa_sequence", sequenceName="pes_seq")
     private Long id;
     private String nome;
     private int idade;
